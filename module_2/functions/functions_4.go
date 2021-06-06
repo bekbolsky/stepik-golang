1aeaf03359fa89d3bb44963f085a124e7e2576c0
Bekbolat Kuralbayev <bkb@live.ru>
2021-06-06 20:00:43 +0600

    refactor: some optimization of fibonacci function

--- module_2/functions/functions_4.go
+++ module_2/functions/functions_4.go
@@ -3,11 +3,8 @@ package main
 import "fmt"
 
 func fibonacci(n int) int {
-	if n == 0 {
-		return 0
-	}
-	if n == 1 {
-		return 1
+	if n == 0 || n == 1 {
+		return n
 	}
 	return fibonacci(n-1) + fibonacci(n-2)
 }
